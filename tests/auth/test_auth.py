fd2b36b9bebe72f372835a90f69329de958bf103
Rufina Shayikhieva <expelliarmus2012@yandex.ru>
2021-09-28 11:27:40 +0300

    Fix test and pre-commit

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -6,13 +6,16 @@ from fixtures.register.model import RegisterUser, RegisterUserResponse
 class TestAuthUser:
     def test_auth_user_with_valid_data(self, app):
         """
-        1. Try to register user with valid data
+        1. Register user with valid data
         2. Check that status code is 201
-        3. Check response
-        #"""
+        3. Try to log in with these data
+        4. Check that status code is 200
+        """
         data = RegisterUser.random()
-        res_register = app.register.register(data=data, type_response=RegisterUserResponse)
+        res_register = app.register.register(
+            data=data, type_response=RegisterUserResponse
+        )
         assert res_register.status_code == 201
-        assert res_register.data_info.message == ResponseText.MESSAGE_REGISTER_USER
+        assert res_register.data.message == ResponseText.MESSAGE_REGISTER_USER
         res_auth = app.auth.auth(data=data, type_response=RegisterAuthResponse)
         assert res_auth.status_code == 200
